89881065bf4924c27b26ffbee607dc12cf683e56
Marcus Vinicius <viniciusguimaraes@ufc.br>
2026-06-18 21:14:54 -0300

    feat: expose option 29 in Main.java for Ford-Fulkerson DFS

--- src/Main.java
+++ src/Main.java
@@ -46,8 +46,9 @@ public class Main {
             System.out.println("24. [Q9-B] Executar MB-DP (Fila Dupla)");
             System.out.println("25. [Q9-C] Executar Threshold (Limiar)");
             System.out.println("26. [Q9-D] Executar Floyd-Warshall (Todos para Todos)");
-            System.out.println("27. [AC03-Q2] Executar Algoritmo de Fluxo Máximo (Edmonds-Karp)");
+            System.out.println("27. [AC03-Q2] Executar Algoritmo de Edmonds-Karp (BFS)");
             System.out.println("28. [AC03-Q2] Gerar Cenário de Fluxo Padrão (10 Vértices, Multígrafo)");
+            System.out.println("29. [AC03-Q5] Executar Algoritmo de Ford-Fulkerson (DFS)");
             System.out.println("----------------------------------------");
             System.out.println("88. [?] COMPARAR G1 e G2 (Isomorfismo/Similaridade)");
             System.out.println("99. [!] ALTERNAR GRAFO ATIVO (Mudar para G" + (indiceAtivo == 0 ? "2" : "1") + ")");
@@ -238,6 +239,15 @@ public class Main {
                     case 28:
                         System.out.println("\n--- GERANDO CENÁRIO DE FLUXO PADRÃO NO G" + (indiceAtivo + 1) + " ---");
                         grafoAtual.gerarCenarioFluxoPadrao();
+                        break;
+
+                    case 29:
+                        System.out.println("\n--- EXECUTANDO FORD-FULKERSON (DFS) NO G" + (indiceAtivo + 1) + " ---");
+                        System.out.print("Digite o ID do vértice de origem (s): ");
+                        int sFF = scanner.nextInt();
+                        System.out.print("Digite o ID do vértice de destino (t): ");
+                        int tFF = scanner.nextInt();
+                        grafoAtual.executarFordFulkersonDFS(sFF, tFF);
                         break;
                     // ------------------------------------------
 
